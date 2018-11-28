41f34ca1c080b051959ce8c4aeba00ca57cc1812
Ivan Polchenko <ipzent@gmail.com>
2018-11-28 12:44:47 -0800

    clone from this repo

--- install.sh
+++ install.sh
@@ -104,7 +104,7 @@ tmpdir=$(mktemp -d)
 
 cd "$tmpdir"
 
-git clone -b "$RELEASE" https://github.com/widdix/aws-ec2-ssh.git
+git clone -b "$RELEASE" https://github.com/i5okie/aws-ec2-ssh.git
 
 cd "$tmpdir/aws-ec2-ssh"
 
